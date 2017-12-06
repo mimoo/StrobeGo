bb2ee650960298172a445d3ffec095ced26f6b3b
david <david@matasano.com>
2017-12-06 11:45:30 +0000

    some tests for the strobe

--- strobe/strobe_test.go
+++ strobe/strobe_test.go
@@ -45,3 +45,22 @@ func TestStream(t *testing.T) {
 		t.Fatal("strobe cannot stream correctly")
 	}
 }
+
+func TestStream2(t *testing.T) {
+	s := InitStrobe("custom string number 2, that's a pretty long string", 128)
+	key := []byte("0101010100100101010101010101001001")
+	s.Operate(false, "KEY", key, 0, false)
+	fmt.Println(s.debugPrintState())
+	s.Operate(false, "KEY", key, 0, true)
+	fmt.Println(s.debugPrintState())
+	message := []byte("hello, how are you good sir? ????")
+	s.Operate(false, "AD", message, 0, false)
+	fmt.Println(s.debugPrintState())
+	s.Operate(false, "AD", message, 0, true)
+	fmt.Println(s.debugPrintState())
+	s.Operate(false, "AD", message, 0, false)
+	fmt.Println(s.debugPrintState())
+	if s.debugPrintState() != "5117b46c2d842655c1be2a69f64f16aaaad2c0050fe2ac5446afe44345a9b10d044c8b3ec8005a9e362c0a431ab5c4d8228c2f890ae56ad3fef4404aa6cc76704b503d627553ae9635d329cdfa86ed29ec0dd79787ff3fcefdee7463c053ef3b4a4fa7c8eb89a6372df2c4ccfc7469d7447bd19a67940642334706e5ff6b1ef58514e55c6b5c6921c58eb7cb5c57978c92c42e598926fcfdcd9705fb948ed6fe9027c65fb0659c98a9c9668d523dfa2b27bde76224944503b686901c989fedac34994dd16daedf00" {
+		t.Fatal("this is not working")
+	}
+}
